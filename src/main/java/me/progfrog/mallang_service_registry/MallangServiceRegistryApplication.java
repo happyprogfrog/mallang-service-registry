a42867acfc37e8ba529a10735398d2f299e141ff
happyprogfrog <happyprogfrog@gmail.com>
2024-07-02 17:02:00 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/mallang_service_registry/MallangServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package me.progfrog.mallang_service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallangServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallangServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/progfrog/mallang_service_registry/MallangServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.mallang_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallangServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
